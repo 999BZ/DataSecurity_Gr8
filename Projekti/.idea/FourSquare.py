52d999855e03f8d13f36d87bebdfdcf50ae1a542
999BZ <blendizeqiri@hotmail.com>
2023-04-04 23:08:30 +0200

    Create FourSquare.py

---
+++ Projekti/.idea/FourSquare.py
@@ -0,0 +1,63 @@
+import string
+from more_itertools import unique_everseen
+import numpy as np
+
+class Matrix:
+    UL=[]
+
+    UR=[]
+
+    DL=[]
+
+    DR=[]
+
+
+    def __init__(self,keyword1,keyword2):
+        self.UR=self.createMatrix(keyword1)
+        self.DL=self.createMatrix(keyword2)
+        self.UL=self.createMatrix()
+        self.DR=self.createMatrix()
+
+    def createMatrix(self,keyword=None):
+        if keyword:
+            keywordNR = ''.join(unique_everseen(keyword))
+            matrix = np.empty((5, 5), dtype='U1')
+            alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
+            temp=0
+            for (i, j), value in np.ndenumerate(matrix):
+                if(temp<len(keywordNR)):
+                    matrix[i,j]=keywordNR[5*i+j].upper()
+                    alphabet = "x" + alphabet
+                    alphabet = alphabet.replace(keywordNR[5*i+j].upper(),'')
+                    temp+=1
+                else:
+                    matrix[i,j]=alphabet[5*i+j]
+            return matrix
+        else:
+            matrix = np.empty((5, 5), dtype='U1')
+            alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
+            for (i, j), value in np.ndenumerate(matrix):
+                matrix[i,j]=alphabet[5*i+j]
+            return matrix
+
+def Encrypt(matrixes,message):
+    if (len(message)%2!=0):
+        message+='x'
+    encryptedMessage=''
+    i1,i2,j1,j2 = 0,0,0,0
+    for k in range(0, len(message),2):
+        for (i,j),value in np.ndenumerate(matrixes.UL):
+            if(matrixes.UL[i,j] == message.upper()[k]):
+                i1=i
+                j1=j
+            if(matrixes.DR[i,j] == message.upper()[k+1]):
+                i2=i
+                j2=j
+        encryptedMessage+=matrixes.UR[i1][j2]+matrixes.DL[i2][j1]
+    return encryptedMessage
+
+
+matrixes = Matrix("secret","keyword")
+message= "HELLOWORLD"
+print("The encrypted message is: ",Encrypt(matrixes,message))
+
